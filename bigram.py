beeffdd19f7550ef22c4f62e72e4bf4bb2c958ea
vanshchapadia17 <chapadiav01@gmail.com>
2026-05-26 10:36:45 +0530

    data folder created

--- bigram.py
+++ bigram.py
@@ -14,7 +14,7 @@ eval_iters = 200
 
 torch.manual_seed(1337)
 
-with open('train.csv', 'r', encoding='utf-8') as f:
+with open('C:\\Users\\DELL\\Desktop\\Transformer\\data\\train.csv', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
